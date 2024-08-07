ba57a8c1dc801dc46915a30c43375f986f60de52
zobjs <experiment.zobaidulkazi@gmail.com>
2024-08-07 16:33:26 +0000

    routes

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,22 @@ const app = express();
 
 app.use(express.json());
 
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+  })
+  
+  app.get("/api", (req, res) => {
+    res.status(200).json({
+      message: "Hello World!",
+      status: 200,
+      success: true,
+      data: null
+    })
+  })
+  
+
+
+
 export default app;
