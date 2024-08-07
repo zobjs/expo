3bb3a6620a0983bcde22cfdb0fa996cda4482e74
zobjs <experiment.zobaidulkazi@gmail.com>
2024-08-07 17:16:31 +0000

    fix: err

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,4 @@ const app = express();
 app.use(express.json());
 
 
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-  })
-  
-  app.get("/api", (req, res) => {
-    res.status(200).json({
-      message: "Hello World!",
-      status: 200,
-      success: true,
-      data: null
-    })
-  })
-  
-
-
-
 export default app;
